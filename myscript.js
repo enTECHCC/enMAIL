a7103045ec104913b2468f44e04c2a5a09e7e9b6
tonyzhang617 <tonyzhang617@gmail.com>
2017-01-17 20:58:57 -0500

    Replaced archive, spam and delete button images with text.

--- myscript.js
+++ myscript.js
@@ -14,15 +14,38 @@ function updateButtons(){
 	if(buttons.length == 0){
 		return;
 	}
+
+	// Remove archive, spam and delete button on the email view
+	var deleteImage =  document.getElementsByClassName("ar9 T-I-J3 J-J5-Ji");
+	if (deleteImage.length > 0) {
+		var deleteImageParent = deleteImage[0].parentNode;
+		deleteImageParent.removeChild(deleteImage[0]);
+		deleteImageParent.innerHTML = "Delete";
+	}
+	
+	var spamImage =  document.getElementsByClassName("asl T-I-J3 J-J5-Ji");
+	if (spamImage.length > 0) {
+		var spamImageParent = spamImage[0].parentNode;
+		spamImageParent.removeChild(spamImage[0]);
+		spamImageParent.innerHTML = "Mark Spam";
+	}
+	
+	var archiveImage = document.getElementsByClassName("ar8 T-I-J3 J-J5-Ji");
+	if (archiveImage.length > 0) {
+		var archiveImageParent = archiveImage[0].parentNode;
+		archiveImageParent.removeChild(archiveImage[0]);
+		archiveImageParent.innerHTML = "Archive";
+	}
+	
 	var i = 0;
-	//iterate through the buttons 
+	//iterate through the buttons
 	for(i=0; i < buttons.length; i++){
 		//replace compose button with new
 		if(buttons[i].textContent === "COMPOSE"){
 			//replace the text of compose button with 'new'
 			buttons[i].textContent = "NEW";
 			console.log('compose button replaced');
-		//remove checkbox button	
+		//remove checkbox button
 		}else if(buttons[i].querySelector("span[role=checkbox]") != null){
 			buttons[i].remove();
 			console.log('checkbox button removed');
@@ -40,6 +63,7 @@ function updateButtons(){
             buttons[i].querySelector("span").textContent = "Older";
         }
 	}
+
 }
 
 // Removes actions checkboxes; starring, selecting, important
@@ -74,22 +98,22 @@ $( document ).ready( function() {
 			emailListLoaded();
 			observer2.observe(targetNode, observerConfig);
 		}
-		    
+
 	});
 
 	// Notify only when nodes are added/deleted
 	var observerConfig = {
-		childList: true, 
+		childList: true,
 		subtree: true
 	};
-	
+
 	//start observing the body of the document for addition of nodes
 	var targetNode = document.body;
 	observer1.observe(targetNode, observerConfig);
-	
+
 	//create an observer to detect switch between email list and email
 	observer2 =  new MutationObserver(function(){
-		
+
 		if(document.body.querySelector("div[role=main] table[role=presentation]") == null){
 			if(listPage!=true){
 				listPage = true;
@@ -100,9 +124,6 @@ $( document ).ready( function() {
 			emailLoaded();
 		}
 	})
-		
-	
+
+
 	});
-
-
-
