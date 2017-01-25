e4ba3bbdd13d93bbbf5c9848574dd40ec887452b
tony <tonyzhang617@gmail.com>
2017-01-24 20:38:45 -0500

    Removed side action buttons by css instead of javascript

--- myscript.js
+++ myscript.js
@@ -15,14 +15,14 @@ function updateButtons(){
 		return;
 	}
 	var i = 0;
-	//iterate through the buttons 
+	//iterate through the buttons
 	for(i=0; i < buttons.length; i++){
 		//replace compose button with new
 		if(buttons[i].textContent === "COMPOSE"){
 			//replace the text of compose button with 'new'
 			buttons[i].textContent = "NEW";
 			console.log('compose button replaced');
-		//remove checkbox button	
+		//remove checkbox button
 		}else if(buttons[i].querySelector("span[role=checkbox]") != null){
 			buttons[i].remove();
 			console.log('checkbox button removed');
@@ -42,26 +42,13 @@ function updateButtons(){
 	}
 }
 
-// Removes actions checkboxes; starring, selecting, important
-function removeSideActions() {
-  $(".k0vOLb").hide(); // first three table columns
-  $(".Ci").hide();
-  $(".y5").hide();
-  $(".oZ-x3.xY").hide(); // checkbox
-  $("td.oZ-x3.xY.aid").hide(); //checkbox
-  $("td.apU.xY").hide(); //star
-  $("td.WA.xY").hide(); //important
-  console.log("remove side actions ran");
-}
 
 function emailLoaded(){
 	updateButtons();
-  removeSideActions();
 }
 
 function emailListLoaded(){
 	updateButtons();
-  removeSideActions();
 }
 
 $( document ).ready( function() {
@@ -74,22 +61,22 @@ $( document ).ready( function() {
 			emailListLoaded();
 			observer2.observe(targetNode, observerConfig);
 		}
-		    
+
 	});
 
 	// Notify only when nodes are added/deleted
 	var observerConfig = {
-		childList: true, 
+		childList: true,
 		subtree: true
 	};
-	
+
 	//start observing the body of the document for addition of nodes
 	var targetNode = document.body;
 	observer1.observe(targetNode, observerConfig);
-	
+
 	//create an observer to detect switch between email list and email
 	observer2 =  new MutationObserver(function(){
-		
+
 		if(document.body.querySelector("div[role=main] table[role=presentation]") == null){
 			if(listPage!=true){
 				listPage = true;
@@ -100,9 +87,6 @@ $( document ).ready( function() {
 			emailLoaded();
 		}
 	})
-		
-	
+
+
 	});
-
-
-
