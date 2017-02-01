6b8f1d450ee5af991527c3df05a1f5981430d7f1
tonyzhang617 <tonyzhang617@gmail.com>
2017-01-31 19:35:28 -0500

    Replaced archive, spam and delete button images with text. (#8)
    * Replaced archive, spam and delete button images with text.
    
    * Abstracted repeated code which replaces images with texts

--- myscript.js
+++ myscript.js
@@ -5,30 +5,43 @@ var listPage = true;
 var observer1;
 var observer2;
 console.log('script started');
+
+function replaceImageWithText(className, newLabel) {
+	var imageToDelete = document.getElementsByClassName(className);
+	if (imageToDelete.length > 0) {
+		var parentElem = imageToDelete[0].parentNode;
+		parentElem.removeChild(imageToDelete[0]);
+		parentElem.innerHTML = newLabel;
+	}
+}
+
 //fuction that replaces the wording of compose button to 'new'
-function updateButtons() {
-    //console.log("updated buttons!")
-    //get all buttons on the page
-    var buttons = document.querySelectorAll("div[role=button]");
-    if (buttons.length == 0) {
-        return;
-    }
-    var i = 0;
-    //iterate through the buttons
-    for (i = 0; i < buttons.length; i++) {
-        //replace compose button with new
-        if (buttons[i].textContent === "COMPOSE") {
-            //replace the text of compose button with 'new'
-            buttons[i].textContent = "NEW";
-            console.log('compose button replaced');
-            //remove checkbox button	
-        }
-        else if (buttons[i].querySelector("span[role=checkbox]") != null) {
-            buttons[i].remove();
-            //console.log('checkbox button removed');
-            //find newer mails button (left arrow button)
-        }
-        else if (buttons[i].getAttribute("data-tooltip") == "Newer") {
+function updateButtons(){
+	//get all buttons on the page
+	var buttons = document.querySelectorAll("div[role=button]");
+	if(buttons.length == 0){
+		return;
+	}
+
+	// Remove archive, spam and delete button on the email view
+	replaceImageWithText("ar9 T-I-J3 J-J5-Ji", "Delete");
+	replaceImageWithText("asl T-I-J3 J-J5-Ji", "Mark Spam");
+	replaceImageWithText("ar8 T-I-J3 J-J5-Ji", "Archive");
+
+	var i = 0;
+	//iterate through the buttons
+	for(i=0; i < buttons.length; i++){
+		//replace compose button with new
+		if(buttons[i].textContent === "COMPOSE"){
+			//replace the text of compose button with 'new'
+			buttons[i].textContent = "NEW";
+			console.log('compose button replaced');
+		//remove checkbox button
+		}else if(buttons[i].querySelector("span[role=checkbox]") != null){
+			buttons[i].remove();
+			console.log('checkbox button removed');
+		//find newer mails button (left arrow button)
+        }else if(buttons[i].getAttribute("data-tooltip") == "Newer"){
             //remove arrow image
             var img = buttons[i].querySelector("img");
             if (img != null) {
@@ -47,7 +60,7 @@ function updateButtons() {
                 buttons[i].querySelector("span").textContent = "Older";
             }
         }
-    }
+	}
 }
 // Removes actions checkboxes; starring, selecting, important
 function removeSideActions() {
@@ -78,6 +91,7 @@ function emailListLoaded() {
     }
     removeSideActions();
 }
+
 $(document).ready(function () {
     //create a mutation observer to observe any additions of nodes
     observer1 = new MutationObserver(function () {
